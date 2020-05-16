8f3dd70ebcb738e091fcc786a6b8cd7e5da5f7cb
AWRamadhan <annaswahyuramadhan01@gmail.com>
2020-05-17 04:14:27 +0700

    Update App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -199,7 +199,7 @@
         <p class="lead">
           In Indonesia there are {all_case.toLocaleString()} people infected
           with COVID-19 with {total_recover.toLocaleString()} recovered from the
-          virus There is {(all_case - (total_recover + total_death)).toLocaleString()}
+          virus. There is {(all_case - (total_recover + total_death)).toLocaleString()}
           people still in in care, that's mean {(((all_case - (total_recover + total_death)) / all_case) * 100).toFixed()}%
           of the patient still in care where mortality rate is in {((total_death / all_case) * 1000).toFixed() / 10}%
           compared to seasonal flu arround 2%. With mortality rate in {((total_death / all_case) * 1000).toFixed() / 10}%
